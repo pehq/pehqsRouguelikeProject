720d40314b26d33f19a92063d5b156a67864b6e9
pehq <128103844+pehq@users.noreply.github.com>
2024-03-31 01:37:13 -0400

    changed width of pre from 800px to 850px to fit 75 characters

--- website/script.js
+++ website/script.js
@@ -46,8 +46,6 @@ function ConvertArrayMapToStr(arraymap) {
     return str;
 }
 
-
-
 const LoadingAnimation = [
     "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n                                     :>                                     \n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
 ]
@@ -55,3 +53,103 @@ const LoadingAnimation = [
 //pre.innerText = "\nI'M IN YOUR WALLS"
 let output = ConvertArrayMapToStr(CreateSquareArrayMap(50, 30))
 pre.innerHTML = output
+
+let animationData = {
+    "FPS" : 8,
+    "Looped" : true,
+    "AnimationType" : "ArrayEdit",
+    "AnimationBase" : [
+        [" "],
+        [" "],
+        [" "],
+        [" "],
+        [" "],
+        [" "],
+        [" "],
+        [" "],
+        [" "],
+        [" "],
+        [" "],
+        [" "],
+        [" "],
+        [" "],
+        [" "],
+        [" "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "],
+        [" "],
+        [" "],
+        [" "],
+        [" "],
+        [" "],
+        [" "],
+        [" "],
+        [" "],
+        [" "],
+        [" "],
+        [" "],
+        [" "],
+        [" "],
+        [" "],
+        [" "]
+    ],
+    "AnimationData" : [
+        {
+            "x" : 38,
+            "y" : 15,
+            "char" : "|"
+        },
+        {
+            "x" : 38,
+            "y" : 15,
+            "char" : "/"
+        },
+        {
+            "x" : 38,
+            "y" : 15,
+            "char" : "-"
+        },
+        {
+            "x" : 38,
+            "y" : 15,
+            "char" : "\\"
+        }
+    ]
+};
+
+console.log(animationData)
+console.log(animationData.AnimationData)
+console.log(animationData.AnimationData[1])
+
+let curFrame = [];
+function StartAnimation(animdata) {
+    let fps = animdata.FPS;
+    let baseHtml = ConvertArrayMapToStr(animdata.AnimationBase);
+    let isLooped = animdata.Looped;
+
+    curFrame = animdata.AnimationBase
+
+    pre.innerHTML = baseHtml;
+}
+
+function UpdateAnimation(frame) {
+    curFrame[frame.y][frame.x] = frame.char
+    
+    pre.innerHTML = ConvertArrayMapToStr(curFrame)
+}
+
+function Animate(animdata) {
+    StartAnimation(animdata);
+
+    let frameNum = 0;
+
+    let timer = setInterval(function() {
+        UpdateAnimation(animdata.AnimationData[frameNum]);
+        if (animdata.AnimationData.length - 1 === frameNum && animdata.Looped == true) {
+            frameNum = 0;
+        } else {
+            frameNum += 1;
+        }
+    }, 1000 / animdata.FPS)
+
+}
+
+Animate(animationData)
